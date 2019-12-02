fbafed77a46feb45e9d424e9ce4aec6b081a7bab
Uladzislau Barouski <uladzislau_barouski@epam.com>
2019-12-02 14:19:55 +0300

    Add section functionality.

--- KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
+++ KatlaSportNg/src/app/hive-management/lists/hive-section-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HiveSectionListItem } from '../models/hive-section-list-item';
 import { HiveService } from '../services/hive.service';
+import { HiveSectionService } from '../services/hive-section.service';
 
 @Component({
   selector: 'app-hive-section-list',
@@ -16,23 +17,24 @@ export class HiveSectionListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private hiveService: HiveService
+    private hiveService: HiveService,
+    private hiveSectionService: HiveSectionService
   ) { }
 
   ngOnInit() {
-  this.route.params.subscribe(p => {
-    this.hiveId = p['id'];
-    this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
-  })
-}
-
-  onDelete(hiveId: number) {
-    var hive = this.hiveSections.find(h => h.id == hiveId);
-    this.hiveService.setHiveStatus(hiveId, true).subscribe(c => hive.isDeleted = true);
+    this.route.params.subscribe(p => {
+      this.hiveId = p['id'];
+      this.hiveService.getHiveSections(this.hiveId).subscribe(s => this.hiveSections = s);
+    })
   }
 
-  onUndelete(hiveId: number) {
-    var hive = this.hiveSections.find(h => h.id == hiveId);
-    this.hiveService.setHiveStatus(hiveId, true).subscribe(c => hive.isDeleted = true);
+  onDelete(hiveSectionId: number){
+    var hiveSection = this.hiveSections.find(h => h.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, true).subscribe(c => hiveSection.isDeleted = true);
+  }
+
+  onUndelete(hiveSectionId: number){
+    var hiveSection = this.hiveSections.find(h => h.id == hiveSectionId);
+    this.hiveSectionService.setHiveSectionStatus(hiveSectionId, false).subscribe(c => hiveSection.isDeleted = false);
   }
 }
--- KatlaSportNg/src/app/hive-management/models/hive-section.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section.ts
@@ -1,3 +1,11 @@
 export class HiveSection {
-    constructor() { }
+  constructor(
+    public id: number,
+    public name: string,
+    public code: string,
+    public storeHiveId: number,
+    public isDeleted: boolean,
+    public lastUpdated: string
+
+  ) { }
 }
